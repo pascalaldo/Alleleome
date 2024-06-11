35e9e1d2e8056416d060f4abcf1a4831b8b8863b
Pascal Pieters <pascalaldo@gmail.com>
2024-06-11 10:31:32 +0200

    Removed mafft and ncbi copies. They should be manually installed by users.

--- setup.py
+++ setup.py
@@ -2,60 +2,6 @@ import subprocess
 
 from setuptools import find_packages, setup
 
-
-def install_blast():
-    try:
-        # Extract the downloaded archive
-        subprocess.check_call(
-            [
-                "tar",
-                "zxvf",
-                "./resources/ncbi-blast-2.14.0+-x64-linux.tar.gz",
-                "-C",
-                "resources/",
-            ]
-        )
-
-        # Add BLAST to the PATH
-        blast_path = "resources/ncbi-blast-2.14.0+/bin"
-        subprocess.check_call(
-            ["echo", 'export PATH="$PATH:' + blast_path + '" >> ~/.bashrc']
-        )
-
-    except subprocess.CalledProcessError:
-        print("Failed to install NCBI BLAST.")
-
-
-def install_mafft():
-    try:
-        # Extract the downloaded archive
-        subprocess.check_call(
-            [
-                "tar",
-                "xvf",
-                "./resources/mafft-7.505-with-extensions-src.tgz",
-                "-C",
-                "resources/",
-            ]
-        )
-
-        mafft_core_dir = "./resources/mafft-7.505-with-extensions/core/"
-        # Make MAFFT executable
-        subprocess.check_call(["make", "clean"], cwd=mafft_core_dir)
-        subprocess.check_call(["make"], cwd=mafft_core_dir)
-        subprocess.check_call(["sudo", "make", "install"], cwd=mafft_core_dir)
-
-    except subprocess.CalledProcessError:
-        print("Failed to install MAFFT.")
-
-
-# Call the BLAST installation function before installing your package
-install_blast()
-
-# Call the MAFFT installation function before installing your package
-install_mafft()
-
-
 setup(
     name="Alleleome",
     version="0.1.0",
