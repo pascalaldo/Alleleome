b4f9f62ab9508135404c3227e592da15370db449
Pascal Pieters <pascalaldo@gmail.com>
2024-06-14 15:55:49 +0200

    Small typo fix

--- Alleleome/preplot/dn_ds.py
+++ Alleleome/preplot/dn_ds.py
@@ -15,7 +15,7 @@ def calculate_dn_ds(codon_muts_path, dn_ds_path):
     df_per_gene.columns = df_per_gene.columns.to_flat_index().map(lambda x: f"{x[1].replace('-', '_')}_count")
     df_per_gene = df_per_gene.reset_index().rename_axis(None, axis=1)
 
-    df_per_gene['dN/dS'] = (df_per_gene['Non_synonymous_count']/a3['Synonymous_count'])
+    df_per_gene['dN/dS'] = (df_per_gene['Non_synonymous_count']/df_per_gene['Synonymous_count'])
 
     df_per_gene = df_per_gene.dropna(axis=0)
     df_per_gene.to_csv(dn_ds_path)
