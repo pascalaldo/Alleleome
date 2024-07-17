470f8273373141ec3ec29f2ee9a89ea3ca637995
Pascal Pieters <pascalaldo@gmail.com>
2024-07-17 13:48:48 +0200

    Profiling fix

--- Alleleome/cli.py
+++ Alleleome/cli.py
@@ -40,10 +40,11 @@ def main_prepare(args):
         load_and_qcqa.load_roary_data(args.summary, args.gp_binary, args.gp_locustag)
     )
     logging.info("Processing GBK files")
-    # all_locustag_df = load_and_qcqa.parse_genbank_files(
-    #     df_gene_presence_locustag, args.gbk_folder,
-    # )
-    cProfile.run('load_and_qcqa.parse_genbank_files(df_gene_presence_locustag, args.gbk_folder)')
+    with cProfile.Profile() as pr:
+        all_locustag_df = load_and_qcqa.parse_genbank_files(
+            df_gene_presence_locustag, args.gbk_folder,
+        )
+        pr.print_stats()
     raise Exception("Debugging")
     logging.info("Preparing QCQA")
     pangene_summary, all_locustag_df, all_genes_df = load_and_qcqa.prepare_qcqa(
